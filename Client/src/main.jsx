6247bd824c93cd62c4dc439f3e396c39ad0a1028
roshanharry <roshandhoni202@gmail.com>
2024-04-17 22:41:25 +0530

    added basic socket functionality

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Import createRoot from "react-dom/client"
+import App from './App.jsx';
+import { CssBaseline } from '@mui/material';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
--- Server/app.js
+++ Server/app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const http = require("http");
+const express = require('express');
+const {Server} = require('socket.io');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = 3000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+    }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(cors());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/', function(req, res) {
+    res.send("Hello world");
+})
 
-module.exports = app;
+io.on("connection", (socket) => {
+    console.log("A new user has connected", socket.id);
+})
+
+
+server.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+})
